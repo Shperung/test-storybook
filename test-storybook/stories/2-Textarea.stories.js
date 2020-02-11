a763c31f137efa313c6d4f4b0f06f239d2fdbac9
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2020-02-11 15:08:04 +0200

    added info

---
+++ test-storybook/stories/2-Textarea.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+addDecorator(withInfo); 
+
+
+export default {
+  title: 'Textarea',
+  parameters: {
+    info: { inline: true },
+  },
+};
+
+export const textarea1 = () => (
+  <div>
+    <textarea onClick={action('clicked')} onChange={action('changed')}>textarea1</textarea>
+    <textarea onClick={action('clicked')} onChange={action('changed')}>textarea1.1</textarea>
+  </div>);
+export const textarea2 = () => <textarea onClick={action('clicked')} onChange={action('changed')}>textarea2</textarea>;

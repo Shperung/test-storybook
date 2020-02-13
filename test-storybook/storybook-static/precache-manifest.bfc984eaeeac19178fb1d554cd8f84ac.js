a05247e159383aec0645871036bdc229eed27ec7
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2020-02-13 10:33:00 +0200

    Tested addon-a11y/register

---
+++ test-storybook/storybook-static/precache-manifest.bfc984eaeeac19178fb1d554cd8f84ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51362c1dd3f7506fae4bcf5c08ee4646",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "aa72eaaace2cee1366bd",
+    "url": "main.css"
+  },
+  {
+    "url": "main.da63785e67597d7b4838.bundle.js"
+  },
+  {
+    "url": "runtime~main.da63785e67597d7b4838.bundle.js"
+  },
+  {
+    "revision": "450628ca2d653bb61f1b",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.da63785e67597d7b4838.bundle.js"
+  },
+  {
+    "url": "vendors~main.da63785e67597d7b4838.bundle.js.LICENSE.txt"
+  }
+]);

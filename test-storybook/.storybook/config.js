e7cca97e5a6f7d8125f6dc69e51978db738c8cd6
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2020-02-13 15:30:06 +0200

    Added theme

--- test-storybook/.storybook/config.js
+++ test-storybook/.storybook/config.js
@@ -1,6 +1,17 @@
 import {setAddon} from '@storybook/react';
 import LiveEdit, {setOptions} from 'storybook-addon-react-live-edit';
 
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming'
+
+
+import pinkPanther from  './pinkPanther';
+addParameters({
+  options: {
+    theme: pinkPanther
+  }
+})
+
 setOptions({ theme: 'darcula', presets: ['react'] });
 
 setAddon(LiveEdit);
---
+++ test-storybook/.storybook/pinkPanther.js
@@ -0,0 +1,29 @@
+import {create} from '@storybook/theming'
+export default create ({
+  base: 'light',
+  colorPrimary: '#f2f5f7',
+  colorSecondary: 'deepskyblue',
+  // UI
+  appBg: '#f2f5f7',
+  appContentBg: 'white',
+  appBorderColor: 'white',
+  appBorderRadius: 4,
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+  // Text colors
+  textColor: '#093454',
+  textInverseColor: '#093454',
+  // Toolbar default and active colors
+  barTextColor: '#093454',
+  barSelectedColor: '#093454',
+  barBg: '#f2f5f7',
+  // Form colors
+  inputBg: '#093454',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
+  brandTitle: 'Pink Panther Storybook',
+  brandUrl: 'https://example.com',
+  brandImage: 'https://static1.squarespace.com/static/58a96e88c534a5fdedb98365/58b6d1f93a04110be820d756/5d87b781a520905cab486c91/1569202240159/wallex.jpg?format=1500w',
+});
